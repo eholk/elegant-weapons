f5c8c6d3003ec97f5b882eb178d80a8094ff491e
Eric Holk <eric.holk@gmail.com>
2011-11-21 18:15:59 -0800

    Using member instead of memq

--- lib/elegant-weapons/sets.scm
+++ lib/elegant-weapons/sets.scm
@@ -1,12 +1,10 @@
 (library
     (elegant-weapons sets)
-  (export union set-add difference intersection member?)
+  (export union set-add difference intersection)
   (import (rnrs))
 
-  (define member? memq)
-
   (define (set-add s x)
-    (if (member? x s)
+    (if (member x s)
         s
         (cons x s)))
   
@@ -16,12 +14,12 @@
   (define (intersection s . s*)
     (fold-left (lambda (s1 s2)
                  (filter (lambda (x)
-                           (member? x s2))
+                           (member x s2))
                          s1))
                s s*))
 
   (define (difference s1 s2)
-    (filter (lambda (x) (not (member? x s2))) s1))
+    (filter (lambda (x) (not (member x s2))) s1))
   
   ;; end library
   )
