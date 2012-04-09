bda5ccc6ce39fc203a434e3bab308352b7e38b70
Eric Holk <eholk@cs.indiana.edu>
2012-04-09 10:36:18 -0400

    Adding cl_mem as a type.

--- lib/elegant-weapons/helpers.scm
+++ lib/elegant-weapons/helpers.scm
@@ -51,80 +51,106 @@
     [(_ (tid id ...) b1 b2 ...)
      (with-syntax ([id (datum->syntax #'tid 'id)] ...)
        b1 b2 ...)]))
+ 
+ ;; This abstracts away most of the boilerplate for writing
+ ;; match-based transformations. A little unsafe.
+ ;; Stipulation: make sure the match macro is in the environment
+ ;; everywhere this macro is used.
+ (define-syntax (define-match x)
+   (syntax-case x ()
+     ((k (name args ...) clauses ...)
+      (with-implicit (k match)
+        #'(define (name args ...)
+            (lambda (arg)
+              (match arg
+                clauses ...
+                (,else
+                  (error 'name "Unrecognized item" else)))))))
+     ((k name clauses ...)
+      (with-implicit (k match)
+        #'(define name
+            (lambda (arg)
+              (match arg
+                clauses ...
+                (,else
+                  (error 'name "Unrecognized item" else)))))))))
 
-;; This abstracts away most of the boilerplate for writing
-;; match-based transformations. A little unsafe.
-;; Stipulation: make sure the match macro is in the environment
-;; everywhere this macro is used.
-(define-syntax define-match
-  (lambda (x)
-    (syntax-case x ()
-      ((k (name args ...) clauses ...)
-       (with-implicit (k match)
-                      #'(define (name args ...)
-                          (lambda (arg)
-                            (match arg
-                              clauses ...
-                              (,else
-                               (error 'name "Unrecognized item" else)))))))
-      ((k name clauses ...)
-       (with-implicit (k match)
-                      #'(define name
-                          (lambda (arg)
-                            (match arg
-                              clauses ...
-                              (,else
-                               (error 'name "Unrecognized item" else))))))))))
+ (define gensym
+   (let ((c 0))
+     (lambda (x)
+       (unless (symbol? x) (error 'gensym "invalid symbol" x))
+       (set! c (+ 1 c))
+       (string->symbol
+         (string-append
+           (symbol->string x) "_" (number->string c))))))
 
-(define gensym
-  (let ((c 0))
-    (lambda (x)
-      (unless (symbol? x) (error 'gensym "invalid symbol" x))
-      (set! c (+ 1 c))
-      (string->symbol
-        (string-append
-          (symbol->string x) "_" (number->string c))))))
+ (define iota
+   (lambda (n)
+     (let loop ([i 0])
+       (cond
+         [(= i n) '()]
+         [else (cons i (loop (+ i 1)))]))))
 
-(define iota
-  (lambda (n)
-    (let loop ([i 0])
-      (cond
-        [(= i n) '()]
-        [else (cons i (loop (+ i 1)))]))))
+ (define andmap
+   (lambda (p ls)
+     (if (null? ls)
+         #t
+         (and (p (car ls)) (andmap p (cdr ls))))))
 
-(define andmap
-  (lambda (p ls)
-    (or (null? ls)
-        (and (p (car ls)) (andmap p (cdr ls))))))
+(define decode-vector-type
+  (lambda (t)
+    (match t
+      ((vec ,[dim t sz] ,len)
+       (values (+ 1 dim) t `(* (int ,len) ,sz)))
+      (,t (values 0 t `(sizeof ,t))))))
 
-(define ormap
-  (lambda (p ls)
-    (and (not (null? ls))
-         (or (p (car ls)) (ormap p (cdr ls))))))
+ (define vector-bytesize
+  (lambda (t)
+    (let-values (((dim t sz) (decode-vector-type t)))
+      sz)))
 
-(define binop?
-  (lambda (op)
-    (case op
-      ((bitwise-or + * - mod /) #t)
-      (else #f))))
+(define-match type-of
+  ((deref ,[e]) e)
+  ((vector-ref ,t ,v ,i) t)
+  ((var ,t ,x) t))
 
-(define relop?
-  (lambda (op)
-    (case op
-      ((< <= = > >=) #t)
-      (else #f))))
+ (define binop?
+   (lambda (op)
+     (case op
+       ((bitwise-or + * - mod /) #t)
+       (else #f))))
+ 
+ (define relop?
+   (lambda (op)
+     (case op
+       ((< <= = > >=) #t)
+       (else #f))))
 
-(define (float? n)
-  (and (number? n) (inexact? n)))
+(define (reserved-word? x)
+  (memq x
+    '(kernel for while print vector vector-ref reduce
+       assert vector-set! set! iota make-vector length)))
 
-(define (scalar-type? t)
-  (case t
-    ((int u64 void str float bool char) #t)
-    (else #f)))
+ (define (ident? x)
+   (and (symbol? x)
+        (not (reserved-word? x))))
+
+ (define (reduceop? op)
+   (memq op '(+ *)))
+
+ (define (float? n)
+   (and (number? n) (inexact? n)))
+ 
+ (define (scalar-type? t)
+   (case t
+     ;; TODO: strings aren't quite scalars, and regions definitely
+     ;; aren't.
+     ((int u64 void str float bool char region_ptr region cl_mem) #t)
+     (else #f)))
 
 (define (c-type? t)
   (case t
-    ((int uint64_t void float char bool cl_mem)
+    ((int uint64_t void float char bool region_ptr region cl_mem)
      #t)
     (else #f)))
 
