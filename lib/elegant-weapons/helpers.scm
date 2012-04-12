c3cdb6dcad17bdcbd427607f8c86abddb74ee0f6
Claire Alvis <claire.alvis@gmail.com>
2012-04-12 17:11:45 -0400

    Would help to remove the merge artifacts

--- lib/elegant-weapons/helpers.scm
+++ lib/elegant-weapons/helpers.scm
@@ -50,7 +50,6 @@
      (with-syntax ([id (datum->syntax #'tid 'id)] ...)
        b1 b2 ...)]))
 
-<<<<<<< HEAD
 ;; This abstracts away most of the boilerplate for writing
 ;; match-based transformations. A little unsafe.
 ;; Stipulation: make sure the match macro is in the environment
