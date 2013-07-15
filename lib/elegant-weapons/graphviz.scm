d808df4369dd64fdd4691402f847eb55233da1db
Eric Holk <eric.holk@gmail.com>
2013-07-15 16:24:00 -0600

    Support clusters in graphviz.

--- lib/elegant-weapons/graphviz.scm
+++ lib/elegant-weapons/graphviz.scm
@@ -5,25 +5,44 @@
    (elegant-weapons helpers))
 
 
-  (define (dot->string g)
-    (string-append
-     "digraph G {\n"
-     (join ""
-           (map (lambda (n)
-                  (let ((s (car n)))
-                    (join ""
-                          (map (lambda (n)
-                                 (string-append
-                                  "    \""
-                                  (symbol->string s)
-                                  "\" -> \""
-                                  (symbol->string n)
+  (define dot->string
+    (case-lambda
+      ((g) (dot->string g '()))
+      ((g c)
+       (string-append
+        "digraph G {\n"
+        (join ""
+              (map (lambda (n)
+                     (let ((s (car n)))
+                       (join ""
+                             (map (lambda (n)
+                                    (string-append
+                                     "    \""
+                                     (symbol->string s)
+                                     "\" -> \""
+                                     (symbol->string n)
                                   "\";\n"))
-                               (cdr n)))))
-                g))
-     "}\n"))
+                                  (cdr n)))))
+                   g))
+        (join
+         "\n"
+         (map (let ((index 0))
+                (lambda (c)
+                  (set! index (+ 1 index))
+                  (string-append "subgraph cluster" (number->string index)
+                                 " {\n"
+                                 (join " "
+                                       (map (lambda (n)
+                                              (string-append
+                                               "\"" (symbol->string n) "\""
+                                               "; "))
+                                            c))
+                                 "\n}\n")))
+              c))
+        "}\n"))))
 
   (define write-dot
     (case-lambda
       ((g) (write-dot g (current-output-port)))
+      ((g c p) (display (dot->string g c) p))
       ((g p) (display (dot->string g) p)))))
